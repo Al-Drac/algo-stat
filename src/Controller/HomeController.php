31acf5a399f89d82b77dee27b20764ebb001cf3e
Theo Tison <tison.theo@gmail.com>
2017-02-01 12:05:27 +0100

    model MVC

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theo
+ * Date: 25/07/16
+ * Time: 15:53
+ */
+
+namespace App\Controller;
+
+require_once './src/Lib/smarty-3.1.29/libs/Smarty.class.php';
+require './src/Model/algo.php';
+
+class HomeController
+{
+    private $Smarty;
+
+    public function __construct()
+    {
+        $this->Smarty = new \Smarty();
+    }
+
+    public function index(){
+        $this->Smarty->display("./src/View/template/home.tpl");
+    }
+}
