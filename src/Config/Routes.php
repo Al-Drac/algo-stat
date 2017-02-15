46fb4486abebfbb9c973fc9e4b1f1273e8eda615
Theo Tison <tison.theo@gmail.com>
2017-02-15 10:50:53 +0100

    add func result

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -7,4 +7,4 @@
  */
 
 $router->get('/', "Home#index");
-/*$router->get('/Recette/:id', "Recette#showRecette")->with('id', '[0-9]+')->with('slug','([a-z\-0-9]+)')*/;
+$router->post('/', "Home#result");
